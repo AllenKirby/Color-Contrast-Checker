52a38b086f6eb8f2edda65930050cb6c4fd2fc72
AllenKirby <130225133+AllenKirby@users.noreply.github.com>
2024-03-21 00:18:04 +0800

    adjust something

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
 const Container = () => {
-    const [color1, setColor1] = useState('');
-    const [color2, setColor2] = useState('');
+    const [color1, setColor1] = useState('#ffffff');
+    const [color2, setColor2] = useState('#000000');
     const [rating, setRating] = useState('');
     const [ratingBgColor, setRatingBgColor] = useState('');
-
+    const [contrast, setContrast] = useState('')
 
     const handleChange = (event, setInputFunction) => {
         setInputFunction(event.target.value);
@@ -60,38 +60,43 @@ const Container = () => {
 
         const contrast = calculateContrastRatio(textColorRGBArray, bgColorRGBArray);
 
+        setContrast(contrast.toFixed(2));
         setRating(calcRating(contrast));
     }, [color1 , color2]) 
 
   return (
     <section className="md:w-1/3 w-full h-auto rounded-2xl shadow-black shadow-2xl bg-gradient-to-br from-yellow-400 to-pink-600 ">
-        <header className="w-full h-auto py-10 flex items-center justify-center"> 
+        <header className="w-full h-auto py-6  flex items-center justify-center"> 
             <p className="md:text-4xl font-bold text-2xl text-white font-mono">COLOR CONTRAST CHECKER</p>
         </header>
         <section className="w-full flex">
-            <div className="px-14 py-5 w-1/2">
-              <div className="text-center p-3 flex items-center justify-center">
-                <div className="p-2 w-auto h-auto">
-                  <div className="w-24 h-24 relative rounded-xl overflow-hidden flex items-center justify-center">
-                    <input type="color" name="color1" id="Color1" className="w-28 h-28 absolute " onChange={(event) => handleChange(event, setColor1)}/> 
-                  </div>
-                  <p className="text-2xl font-semibold my-3 text-white">{color1 ? color1 : '#000000'}</p>
+          <div className="px-14 py-5 w-1/2">
+            <div className="p-2 w-auto h-auto text-center">
+              <p className="text-2xl font-medium text-white">Font Color</p>
+              <div className="w-full flex items-center justify-center">
+                <div className="w-24 h-24 relative rounded-xl overflow-hidden flex items-center justify-center">
+                  <input type="color" name="color1" id="Color1" value={color1} className="w-28 h-28 absolute" onChange={(event) => handleChange(event, setColor1)}/> 
                 </div>
               </div>
+              <p className="text-2xl font-semibold my-3 text-white">{color1}</p>
             </div>
-            <div className="px-14 py-5 w-1/2">
-              <div className="text-center p-3 flex items-center justify-center">
-                <div className="p-2 w-auto h-auto">
-                  <div className="w-24 h-24 relative rounded-xl overflow-hidden flex items-center justify-center">
-                    <input type="color" name="color2" id="Color1" className="w-28 h-28 absolute" onChange={(event) => handleChange(event, setColor2)}/>
-                  </div>
-                  <p className="text-2xl font-semibold my-3 text-white">{color2 ? color2 : '#000000'}</p>
+          </div>
+          <div className="px-14 py-5 w-1/2">
+            <div className="p-2 w-auto h-auto text-center">
+              <p className="text-2xl font-medium text-white">BG Color</p>
+              <div className="w-full flex items-center justify-center">
+                <div className="w-24 h-24 relative rounded-xl overflow-hidden flex items-center justify-center">
+                  <input type="color" name="color2" id="Color1" value={color2} className="w-28 h-28 absolute" onChange={(event) => handleChange(event, setColor2)}/>
                 </div>
               </div>
+              <p className="text-2xl font-semibold my-3 text-white">{color2}</p>
             </div>
+          </div>
         </section>
-        <section className="w-full h-aut  o px-14 py-8 flex items-center justify-center">
-            <p className={`text-white text-3xl font-bold px-8 py-9 ${ratingBgColor} rounded-2xl`}>{rating}</p>
+        <section className="w-full h-auto px-14 pb-8 pt-3 text-center">
+          <p className="rounded-xl py-4 px-9 text-xl" style={{backgroundColor: color2, color: color1}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <p className="text-white text-6xl my-5">{contrast}</p>
+          <p className={`text-white text-3xl font-bold px-8 py-9 ${ratingBgColor} rounded-2xl`}>{rating}</p>
         </section>
     </section>
   )
